630fe0fa0a7d3d69479bf506dedc4d5f35268727
Shark0oSs <louis.nectoux2@gmail.com>
2024-09-25 15:47:50 +0200

    FEAT CGV

---
+++ src/Controller/LegalController.php
@@ -0,0 +1,17 @@
+<?php
+
+// src/Controller/LegalController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LegalController extends AbstractController
+{
+    #[Route('/terms', name: 'app_terms')]
+    public function terms(): Response
+    {
+        return $this->render('legal/terms.html.twig');
+    }
+}
